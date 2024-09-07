fa241829f8b7f38bf1d4eb151968e91644328073
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-09-08 01:34:00 +0300

    emoji

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://wallet.b.goit.study/api';
+const API_URL = 'https://wallet.b.goit.study';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
@@ -8,7 +8,10 @@ const axiosInstance = axios.create({
 
 export const signInUser = async (email, password) => {
   try {
-    const response = await axiosInstance.post('/auth/sign-in', { email, password });
+    const response = await axiosInstance.post('api/auth/sign-in', {
+      email,
+      password,
+    });
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : { message: 'Server error' };
@@ -17,7 +20,11 @@ export const signInUser = async (email, password) => {
 
 export const signUpUser = async (name, email, password) => {
   try {
-    const response = await axiosInstance.post('/auth/sign-up', { name, email, password });
+    const response = await axiosInstance.post('api/auth/sign-up', {
+      name,
+      email,
+      password,
+    });
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : { message: 'Server error' };
